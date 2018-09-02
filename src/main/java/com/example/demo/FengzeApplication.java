e623fc24893605a4bc857ac99ff8539facc5e0de
feinix <yuyx0523@gmail.com>
2018-09-02 23:14:55 +0800

    init files

---
+++ src/main/java/com/example/demo/FengzeApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FengzeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FengzeApplication.class, args);
+	}
+}
